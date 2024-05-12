f146f1e189ad545e40aafa8d2f85deee6a59ac45
krishnamadugu <krishnamusalimadugu@gmail.com>
2024-05-12 09:12:40 +0530

    rock paper scissors project

---
+++ js/sample.js
@@ -0,0 +1,10 @@
+const student = {
+    name : 'krishna',
+    class : 'k19pg'
+};
+
+student.name = 20;
+student["fullNamme"] = "krishna madugu";
+
+console.log(student);
+console.log(student.name);
---
+++ rock-paper-scissors/app.js
@@ -0,0 +1,53 @@
+let userCount = 0; 
+let compCount = 0;
+
+let userChoices = document.querySelectorAll('.choice');
+let btnText = document.querySelector('#msg');
+let userScoreText = document.querySelector('#user_score');
+let compScoreText = document.querySelector('#comp_score');
+
+userChoices.forEach((val) => {   
+    val.addEventListener('click', () => {
+        let userChoice = val.getAttribute('id');
+        console.log("button clicked ",val.getAttribute('id'));
+        console.log(userChoices);
+
+        let compChoice = genCompChoice();
+       console.log("computer choice : ",compChoice);
+       console.log("computer choice index : ",compChoice);
+
+        if(userChoice === compChoice ){
+            //draw Game
+            console.log("computer choice : ",compChoice);
+            btnText.innerText = "Draw game";
+            btnText.style.backgroundColor = "#000000";
+        } else {
+            let userWin = true;
+            if(userChoice === 'rock') {
+                userWin  = compChoice === 'paper' ? false : true;
+            } else if(userChoice === 'paper') {
+                userWin = compChoice === 'scissors' ? false : true;
+            } else {
+                userWin = compChoice === 'rock' ? false : true;
+            }
+            if(userWin) {
+                userCount++;
+                btnText.innerText = "user wins";
+                btnText.style.backgroundColor = "green";
+                userScoreText.innerText = userCount;
+            } else {
+                compCount++;
+                compScoreText.innerText = compCount;
+                btnText.innerText = "Commputer wins";
+                btnText.style.backgroundColor = "red";
+            }
+        }
+    });
+});
+
+
+let genCompChoice = () => {
+    let options = ['rock','paper','scissors'];
+    let randIndx =  Math.floor(Math.random() * 3);
+    return options[randIndx];
+}
